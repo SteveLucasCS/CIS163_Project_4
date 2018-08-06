b06d6bf2aaa03b08ea98f2feac73cd802aefaf90
SteveLucasCS <SteveLucasCS@gmail.com>
2018-08-06 19:43:58 -0400

    done

--- src/project4/DNode.java
+++ src/project4/DNode.java
@@ -1,66 +1,58 @@
-package project4;
-
-/******************************************************************
-*DoubleLinkedList Node object. Stores a reference to a DVD and
-*reference to next and previous nodes.
-*@author Steve Lucas
-*@version 7/18/2018
-*****************************************************************/
 
 public class DNode {
 	/**
-	 * Reference to a DVD object.
+	 * Reference to an object.
 	 */
-	DVD dvd;
-	
+	 Object obj;
+
 	/**
 	 * Reference to the next node in the list
 	 */
 	DNode next;
-	
+
 	/**
 	 * Reference to the previous node in the list.
 	 */
 	DNode prev;
-	
-	
+
+
 	/******************************************************************
-	*Default constructor. Creates a node with all field variables 
+	*Default constructor. Creates a node with all field variables
 	*set to null.
 	******************************************************************/
 	public DNode() {
 		//blank constructor
-		this.dvd = null;
+		this.obj = null;
 		this.next = null;
 		this.prev = null;
 	}
-	
+
 	/******************************************************************
-	*Constructor accepting a DVD object as the only parameter. Sets
+	*Constructor accepting an object as the only parameter. Sets
 	*the node's dvd variable to given DVD, other variables set to null
-	*@param DVD - Reference to a DVD object this node will represent
+	*@param Object - Reference to object this node will represent
 	******************************************************************/
-	public DNode(DVD dvd) {
+	public DNode(Object obj) {
 		//constructor with no links input
-		this.dvd = dvd;
+		this.obj = obj;
 		this.next = null;
 		this.prev = null;
 	}
-	
+
 	/******************************************************************
 	*Constructor accepting an input for each instance variable and
 	*setting them to respective input.
-	*@param DVD - Reference to a DVD object this node will reperesent
+	*@param Object - Reference to object this node will reperesent
 	*@param DNode next - Reference to the next node in the list
 	*@param DNode prev - Reference to the previous node in the list
 	******************************************************************/
-	public DNode(DVD dvd, DNode next, DNode prev) {
+	public DNode(Object obj, DNode next, DNode prev) {
 		//constructor with all fields input
-		this.dvd = dvd;
+		this.obj = obj;
 		this.next = next;
 		this.prev = prev;
 	}
-	
+
 	/******************************************************************
 	*Sets this.next to given DNode reference. Next node in the list.
 	*@param DNode nextNode - The node this.next will point to.
@@ -68,7 +60,7 @@ public class DNode {
 	public void setNext(DNode nextNode) {
 		this.next = nextNode;
 	}
-	
+
 	/******************************************************************
 	*Sets this.prev to given DNode reference. Previous node in the list.
 	*@param DNode prevNode - The node this.prev ill point to.
@@ -76,7 +68,7 @@ public class DNode {
 	public void setPrev(DNode prevNode) {
 		this.prev = prevNode;
 	}
-	
+
 	/******************************************************************
 	*Returns this nodes reference to the next node in the list.
 	*@returns Dnode next - returns value of this.next
@@ -84,7 +76,7 @@ public class DNode {
 	public DNode getNext() {
 		return this.next;
 	}
-	
+
 	/******************************************************************
 	*Returns this nodes reference to the previous node in the list.
 	*@returns Dnode prev - returns value of this.prev
@@ -92,27 +84,27 @@ public class DNode {
 	public DNode getPrev() {
 		return this.prev;
 	}
-	
+
 	/******************************************************************
 	 * Sets the value of which DVD object this node will reference
 	 * @param DVD - DVD object that this node will reference
 	 * @return boolean - true if DVD set properly, false if failed
 	 ******************************************************************/
-	public boolean setDVD(DVD dvd) {
-		if (dvd != null) {
-			this.dvd = dvd;
+	public boolean setObj(Object obj) {
+		if (obj != null) {
+			this.obj = obj;
 			return true;
 		}
 		else
 			return false;
 	}
-	
+
 	/******************************************************************
 	*Returns this node's DVD object reference
 	*@returns DVD - the dvd object this node represents
 	******************************************************************/
-	public DVD getDVD() {
-		return this.dvd;
+	public Object getObj() {
+		return this.obj;
 	}
-	
+
 }
